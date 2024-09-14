c4b055dd623f18b2f5ba40ab307d94e1682c13a3
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2024-09-14 06:45:54 +0000

    Added the function that prints the pairs of letters from a given name

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def shortest_longest(names):
     shortest_name, longest_name = min(names, key=len), max(names, key=len)  # Find the shortest name by length # Find the longest name by length
     return shortest_name, longest_name # returns the names in a list
 
+def letter_pairs(name):
+    pairs = []
+    for i in range(len(name) - 1):
+        pairs.append([name[i], name[i+1]])  # Create a pair and add to the list
+    return pairs
 
 with open('names.txt', 'r') as file:
     names = file.readlines()  # Reads all lines into a list
     names = [name.strip() for name in names]  # Removes trailing newline characters
 
-print(f"\nShortest name: {shortest_longest(names)[0]}\nLongest name: {shortest_longest(names)[1]}\n")
+#print(f"\nShortest name: {shortest_longest(names)[0]}\nLongest name: {shortest_longest(names)[1]}\n")
+
+#name = input("Give a name: ")
+#print(letter_pairs(name))
