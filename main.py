3d425728abb58d0466ad47983b06911e4eb16691
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2024-09-14 06:48:46 +0000

    modifide the function to display the first and last letter in the name

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def shortest_longest(names):
     shortest_name, longest_name = min(names, key=len), max(names, key=len)  # Find the shortest name by length # Find the longest name by length
     return shortest_name, longest_name # returns the names in a list
 
-def letter_pairs(name):
+def pairs(name):
     pairs = []
     for i in range(len(name) - 1):
         pairs.append([name[i], name[i+1]])  # Create a pair and add to the list
-    return pairs
+    start_letter, end_letter = name[0], name[-1]  # First letter of the name # Last letter of the name
+    return pairs, start_letter, end_letter
 
 with open('names.txt', 'r') as file:
     names = file.readlines()  # Reads all lines into a list
@@ -14,5 +15,6 @@ with open('names.txt', 'r') as file:
 
 #print(f"\nShortest name: {shortest_longest(names)[0]}\nLongest name: {shortest_longest(names)[1]}\n")
 
-#name = input("Give a name: ")
-#print(letter_pairs(name))
+name = input("Give a name: ")
+letter_pairs, start, end = pairs(name)
+print(f"\nLetter pairs: {letter_pairs}\nStart letter: {start}\nEnd letter: {end}\n")
