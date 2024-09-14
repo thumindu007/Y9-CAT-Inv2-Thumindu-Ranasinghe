3fcf09f040ab69ee5e072b3f089ac5c7ae48e1c3
DrDuck_27 <83153830+thumindu007@users.noreply.github.com>
2024-09-14 04:28:14 +0000

    created a way to open the text file and put the names into a list

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 
+
+
+with open('names.txt', 'r') as file:
+    names = file.readlines()  # Reads all lines into a list
+    names = [name.strip() for name in names]  # Removes trailing newline characters
+
